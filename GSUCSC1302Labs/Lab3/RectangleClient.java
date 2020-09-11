c846b2a7fa05d960ca613be29de640a3429fd1fa
preethamjava123 <46249739+preethamjava123@users.noreply.github.com>
2020-09-11 15:28:10 -0400

    Update RectangleClient.java

--- GSUCSC1302Labs/Lab3/RectangleClient.java
+++ GSUCSC1302Labs/Lab3/RectangleClient.java
@@ -3,17 +3,12 @@ package csc1302;
  * We are implementing the over-ridden toString() method
  * */
 public class RectangleClient {
-
 	public static void main(String[] args) {
 		Rectangle rect1 = new Rectangle();
-		Rectangle rect2 = new Rectangle();
-		
+		Rectangle rect2 = new Rectangle();	
 		rect1.setFields(2, 13, 14, 5);
-		rect2.setFields(6, 9, 36, 98);
+		rect2.setFields(6, 9, 3, 8);
 		System.out.println("rect1: " + rect1.toString());
 		System.out.println("rect2: " + rect2.toString());
-		
 	}
-
 }
-
