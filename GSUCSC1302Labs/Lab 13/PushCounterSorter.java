228c255d9ceb04b12212081f265af402c5ac37d7
preethamjava123 <46249739+preethamjava123@users.noreply.github.com>
2020-11-17 14:55:21 -0500

    Add files via upload

---
+++ GSUCSC1302Labs/Lab 13/PushCounterSorter.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package csc1302;
+
+/**
+ * @author sthelluri1
+ *
+ */
+import javax.swing.*;
+import java.awt.*;
+public class PushCounterSorter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Sorter");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		PushCounterPanelSorter panel = new PushCounterPanelSorter();
+		frame.getContentPane().add(panel);
+		
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+}
+
