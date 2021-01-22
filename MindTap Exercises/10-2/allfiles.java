e806c785bcd1939f58de942990a89c8a3ed2d220
Preetham Thelluri <46249739+preethampython101@users.noreply.github.com>
2021-01-22 14:44:37 -0500

    Update allfiles.java

--- MindTap Exercises/10-2/allfiles.java
+++ MindTap Exercises/10-2/allfiles.java
@@ -1,35 +1,60 @@
-public class DebugVacation
+public class Candle
 {
-   protected int days;
-   public DebugVacation()
+    int height;
+    double price;
+    private String color;
+
+    public void setHeight(int h){
+        height = h;
+        price = 2.0 * h;
+    }
+    public void setColor(String col){
+        color = col;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public int getHeight(){
+        return height;
+    }
+    public String getColor(){
+        return color;
+    }
+}
+
+public class DemoCandles
+{
+   public static void main(String args[])
    {
-      days = 10;
-   }
-   public int getDays()
-   {
-      return days;
+      Candle aCandle = new Candle();
+      ScentedCandle aScentedCandle = new ScentedCandle();
+      aCandle.setColor("pink");
+      aCandle.setHeight(6);
+      aScentedCandle.setColor("white");
+      aScentedCandle.setScent("gardenia");
+      aScentedCandle.setHeight(6);
+      System.out.println("The " + aCandle.getHeight() +
+         " inch " + aCandle.getColor() +
+         " candle costs $" + aCandle.getPrice());
+      System.out.println("The " + aScentedCandle.getHeight() + " inch " +
+         aScentedCandle.getScent() +
+         " " + aScentedCandle.getColor() +
+         " candle costs $" + aScentedCandle.getPrice());
    }
 }
 
-// A Vacation is 10 days
-// but an ExtendedVacation is 30 days
-public class DebugTen2
+public class ScentedCandle extends Candle
 {
-   public static void main(String args[]) 
-   {
-      DebugVacation myVacation = new DebugVacation();
-      DebugExtendedVacation yourVacation =
-         new DebugExtendedVacation();
-      System.out.println("My vacation is for " +
-         myVacation.getDays() + " days");
-      System.out.println("Your vacation is for " +   yourVacation.getDays() + " days");
-   }
-}
+    private String scent;
 
-public class DebugExtendedVacation extends DebugVacation
-{
-   public DebugExtendedVacation()
-   {
-      super.days = 30;
-   }
+    public void setScent(String sce){
+        scent = sce;
+    }
+    public String getScent(){
+        return scent;
+    }
+    public void setHeight(int hei){
+        super.height = hei;
+        super.price = 3.0 * super.height;
+    }
 }
